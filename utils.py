241934bf13a9b4856361928460fe6bff28085f48
lucaskyle <lucasspecialk@hotmail.com>
2019-12-18 12:49:15 +0900

    merge with master

--- utils.py
+++ utils.py
@@ -1,32 +1,37 @@
+import os
+import torch
+import torchvision.transforms as transforms
+
+
 class AvgrageMeter(object):
 
-  def __init__(self):
-    self.reset()
+    def __init__(self):
+        self.reset()
 
-  def reset(self):
-    self.avg = 0
-    self.sum = 0
-    self.cnt = 0
+    def reset(self):
+        self.avg = 0
+        self.sum = 0
+        self.cnt = 0
 
-  def update(self, val, n=1):
-    self.sum += val * n
-    self.cnt += n
-    self.avg = self.sum / self.cnt
+    def update(self, val, n=1):
+        self.sum += val * n
+        self.cnt += n
+        self.avg = self.sum / self.cnt
 
 
 def accuracy(output, label, topk=(1,)):
-  maxk = max(topk)
-  batch_size = label.size(0)
+    maxk = max(topk)
+    batch_size = label.size(0)
 
-  _, pred = output.topk(maxk, 1, True, True)
-  pred = pred.t()
-  correct = pred.eq(label.view(1, -1).expand_as(pred))
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(label.view(1, -1).expand_as(pred))
 
-  res = []
-  for k in topk:
-    correct_k = correct[:k].view(-1).float().sum(0)
-    res.append(correct_k.mul_(100.0/batch_size))
-  return res
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
 
 
 def save_checkpoint(state, iters, tag=''):
@@ -34,4 +39,27 @@ def save_checkpoint(state, iters, tag=''):
         os.makedirs("./snapshots")
     filename = os.path.join("./snapshots/{}checkpoint-{:06}.pth.tar".format(tag, iters))
     torch.save(state, filename)
-    
+
+
+def _data_transforms_cifar10(args):
+    """from darts
+    https://github.com/quark0/darts/blob/master/cnn/utils.py
+    """
+
+    CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
+    CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
+
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+    ])
+    # if args.cutout:
+    #   train_transform.transforms.append(Cutout(args.cutout_length))
+
+    valid_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
+    ])
+    return train_transform, valid_transform
